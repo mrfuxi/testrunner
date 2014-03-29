473f217cb8d7223244073976f480e76903b91617
Karol Dulęba <mr.fuxi@gmail.com>
2014-03-29 22:00:22 +0000

    Replace requirements with setup.py. Start writing README.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Test Runner
 ==========
 Simple Python module to run test when your code changes.
 It can be used for TDD, modifing code will cause execution of tests
+
+Instalation
+-----------
+
+Basic Usage
+-----------
+
+    python -m testrunner
+
+Features
+--------
+
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name="Test Runner",
+    version="0.1",
+    author="Karol Duleba",
+    author_email="mr.fuxi@gmail.com",
+    url="https://github.com/mrfuxi/testrunner",
+    license="MIT",
+    description="Test runner for TDD",
+    long_description=open("README.md").read(),
+    packages=["testrunner"],
+    install_requires=[
+        "nose-notify == 0.4.2",
+        "pexpect == 3.1",
+        "pyinotify == 0.9.4",
+    ],
+)
